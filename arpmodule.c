65339052b6d499c33d84ed266ee9194046732996
Adrian <adr.luk.git@wp.pl>
2023-04-18 14:39:24 +0200

    main driver refactor, to be done

--- arpmodule.c
+++ arpmodule.c
@@ -1,26 +1,90 @@
-#include <sys/socket.h>
-#include <linux/if_arp.h>
+// #include <sys/socket.h>
+// #include <linux/if_arp.h>
 
-#include <arpa/inet.h> // htons
+#include <unistd.h>
+
+
+
+// #include <arpa/inet.h>
+// #include <sys/socket.h>
+
 
 #include "arpmodule.h"
 
 int main() {
-    uint8_t s_mac[6] = {213, 1, 162, 184, 17, 93};
-    uint8_t d_mac[6] = {121, 121, 121, 121, 121, 121};
-    uint8_t s_ip[6] = {192, 168, 12, 174};
-    uint8_t d_ip[6] = {121, 121, 121, 121};
+    struct sockaddr_ll device;
+    char *interface = "enp0s25";
+    int bytes;
 
-    arp_header areq = ARPRequest(s_mac, d_mac, s_ip, d_ip);
+    uint8_t s_mac[6] = {0x90, 0x1b, 0x0e, 0x85, 0x1d, 0x3b};
+    // uint8_t d_mac[6] = {121, 121, 121, 121, 121, 121};
+    uint8_t s_ip[4] = {192, 168, 0, 134};
+    uint8_t d_ip[4] = {192, 168, 0, 108};
 
+    // arp_header areq = ARPRequest(s_mac, s_ip, d_ip);
 
-    dump_readable_packet(&areq);
+    uint8_t *arp_req = arpreq(s_mac, s_ip, d_ip);
+
+    // dump_readable_packet(&areq);
 
     int arp_socket;
 
+    memset (&device, 0, sizeof (device));
+    if ((device.sll_ifindex = if_nametoindex(interface)) == 0) {
+        perror ("if_nametoindex() failed to obtain interface index ");
+        exit (EXIT_FAILURE);
+    }
+    device.sll_family = AF_PACKET;
+    memcpy (device.sll_addr, s_mac, ETH_ALEN);
+    device.sll_halen = 6;
+
+
+    if ((arp_socket = socket (PF_PACKET, SOCK_RAW, htons (ETH_P_ALL))) < 0) {
+        perror ("socket() failed ");
+        exit (EXIT_FAILURE);
+    }
+    // for(int i = 0)
+    if ((bytes = sendto (arp_socket, arp_req, sizeof(struct _arp_header), 0, (struct sockaddr *) &device, sizeof (device))) <= 0) {
+        perror ("sendto() failed");
+        exit (EXIT_FAILURE);
+    } 
+
+
     // if(arp_socket = socket(AF_NETLINK))
 
+    close(arp_socket);
+    free(arp_req);
+
+    printf("sent.\n");
     return 0;
+}
+
+
+
+uint8_t *arpreq(uint8_t *src_mac, uint8_t *src_ip, uint8_t *dst_ip) {
+
+    /* pierwsze co, dodanie naglowka ethernet */
+    struct ethhdr eth_hdr;
+
+    /* w ARP mac dest jest adresem broadcast */
+    memset(eth_hdr.h_dest, 0xff, ETH_ALEN);
+
+    /* mac src to mac maszyny */
+    memcpy(eth_hdr.h_source, src_mac, ETH_ALEN);
+
+    /* proto dla ARP to 0x0806 */
+    eth_hdr.h_proto = htons(ETH_P_ARP);
+ 
+    struct _arp_header arphdr = ARPRequest(src_mac, eth_hdr.h_dest, src_ip, dst_ip);     
+
+    // uint8_t *req = malloc(sizeof(struct _arp_header) + sizeof(eth_hdr));
+
+
+    // memcpy(req, &eth_hdr, sizeof(eth_hdr));
+    // memcpy(req + sizeof(eth_hdr), &arphdr, sizeof(arphdr));
+    uint8_t *req = malloc(sizeof(struct _arp_header));
+    memcpy(req, &arphdr, sizeof(arphdr));
+    return req;
 }
 
 arp_header ARPRequest(uint8_t src_mac[6], uint8_t dst_mac[6],
@@ -28,12 +92,16 @@ arp_header ARPRequest(uint8_t src_mac[6], uint8_t dst_mac[6],
     //inicjalizacja pakietu ARP dla ethernetu
 
     arp_header arp = {
+        .eth_type = htons(ETH_P_ARP),
         .htype = htons(ARPHRD_ETHER),
         .hlen = 6,
         .ptype = htons(0x0800),  //IPv4
         .plen = 4,
         .opcode = htons(ARPOP_REQUEST)
     };
+
+    memcpy(arp.eth_shost, src_mac, arp.hlen);
+    memcpy(arp.eth_dhost, dst_mac, arp.hlen);
 
     memcpy(arp.src_mac, src_mac, arp.hlen);
     memcpy(arp.dst_mac, dst_mac, arp.hlen);
--- arpmodule.h
+++ arpmodule.h
@@ -5,10 +5,35 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include <stdint.h>
+// #include <stdint.h>
+
+// // #include <linux/if.h>
+// #include <linux/if_ether.h>
+
+// #include <net/if.h> //if_nametoindex
+
+#include <netdb.h>            // struct addrinfo
+#include <sys/types.h>        // needed for socket(), uint8_t, uint16_t
+#include <sys/socket.h>       // needed for socket()
+#include <netinet/in.h>       // IPPROTO_RAW, INET_ADDRSTRLEN
+#include <netinet/ip.h>       // IP_MAXPACKET (which is 65535)
+#include <arpa/inet.h>        // inet_pton() and inet_ntop()
+#include <sys/ioctl.h>        // macro ioctl is defined
+#include <bits/ioctls.h>      // defines values for argument "request" of ioctl.
+#include <net/if.h>           // struct ifreq
+#include <linux/if_ether.h>   // ETH_P_ARP = 0x0806
+#include <linux/if_packet.h>  // struct sockaddr_ll (see man 7 packet)
+#include <net/ethernet.h>
+
+#include <linux/if_arp.h>
+
 
 
 struct _arp_header {
+    uint8_t eth_dhost[ETH_ALEN];
+    uint8_t eth_shost[ETH_ALEN];
+    uint16_t eth_type;
+
     uint16_t htype; //hardware type, for ethernet is 1
     uint16_t ptype; //protocol of higher layer
     uint8_t hlen; //hardware address len, for ether 6 (MAC addr)
@@ -26,6 +51,8 @@ typedef struct _arp_header arp_header;
 arp_header ARPRequest(uint8_t src_mac[6], uint8_t dst_mac[6],
                     uint8_t src_ip[4], uint8_t dst_ip[4]);
 arp_header ARPResponse();
+
+uint8_t *arpreq(uint8_t *src_mac, uint8_t *src_ip, uint8_t *dst_ip);
 
 void dump_readable_packet(const arp_header *hdr);
 
