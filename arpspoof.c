b502e8772e70ae0c7f943aab53dd55270fe4aeaa
Adrian <adr.luk.git@wp.pl>
2023-04-26 14:38:05 +0200

    code refactor

--- arpspoof.c
+++ arpspoof.c
@@ -3,8 +3,37 @@
 
 #include "arpspoof.h"
 
-void schedule_queue() {
+void spoofer(int argc, char *argv[]) {
+    if(getuid != 0) {
+        printf("Uruchom jako root, wychodzenie z programu...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    uint8_t victim_ip[4];
+    memset(victim_ip, 0, sizeof(victim_ip));
+
+    if(argc != 1) {
+        struct sockaddr_in sa;
+        if(!inet_pton(AF_INET, argv[1], &(sa.sin_addr))) {
+            printf("Niewlasciwy docelowy adres IP. Uruchom program z poprawnym adresem IP, konczenie...\n");
+            exit(EXIT_FAILURE);
+        }
+        transform_ip(victim_ip, sa.sin_addr.s_addr);
+    }
+
     
+}
+
+void transform_ip(uint8_t *ip, uint32_t ip_decimal) {
+    ip[0] = ip_decimal & 0xff;
+    ip[1] = (ip_decimal >> 8) & 0xff;
+    ip[2] = (ip_decimal >> 16) & 0xff;
+    ip[3] = (ip_decimal >> 24) & 0xff;
+}
+
+void schedule_queue() {
+    uint8_t victim_ip[4];
+
 }
 
 uint8_t *rand_mac(uint8_t *mac) {
--- arpspoof.h
+++ arpspoof.h
@@ -5,10 +5,13 @@
 
 #include "arpmodule.h"
 
+void spoofer(int argc, char *argv[]);
+
 void schedule_queue();
 
 void rng(uint8_t *data, const int min, const int max, const int nums);
 
+void transform_ip(uint8_t *ip, uint32_t ip_decimal);
 uint8_t *rand_mac(uint8_t *mac);
 
 
